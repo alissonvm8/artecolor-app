3d39f49c07c4a98b8b51af43d51c4c2da8650576
alissonvm8 <alissonvmp@gmail.com>
2025-06-15 17:59:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
 
-st.set_page_config(page_title="Dashboard Arte Color", layout="wide")
-
-st.title("📊 Dashboard Comercial - Arte Color")
+st.title("Hola, Arte Color ")
+st.write("Primera app de prueba")
 
